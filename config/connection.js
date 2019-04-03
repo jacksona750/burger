1fc4dab3fe18e74c00fc1f0f4f3ab3b82e281762
Jacksona750 <jacksona750@gmail.com>
2019-04-02 20:54:25 -0700

    fixing another error

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
 var mysql = require("mysql");
-
 var connection;
 
 if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWS_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
     host: "localhost",
